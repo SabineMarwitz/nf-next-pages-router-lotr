f4e7084d35f9163230713940d914460ef0a9a6ee
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-08-13 15:10:05 +0200

    added 3 volume pages and content

---
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,23 @@
+import { volumes } from "../../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+const mySlug = "the-fellowship-of-the-ring";
+
+const content = volumes.find(({slug}) => slug === mySlug);
+
+export default function handler() {
+    return <>
+        <Link href="/">All Volumes</Link>
+        <h1>{ content.title }</h1>
+        <p>{ content.description }</p>
+        <ul>{content.books.map((b) => <li> {b.ordinal} {b.title} </li> )}</ul>
+        <Image 
+            src={content.cover}
+            height={230}
+            width={140}
+            alt="A cover picture"
+        />;
+    </>
+}
+
